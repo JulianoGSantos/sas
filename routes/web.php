48bdfd4d6e12953b570d3ea27347259d2946ddd1
juliano <jot.asa@hotmail.com>
2024-05-14 00:54:36 -0300

    complete

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('auth.login');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
